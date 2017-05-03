1b89a8fb2d76358cbc849115ee186d8d501e2a06
Jishnu Raj <jishnurajm@gmail.com>
2017-05-03 19:40:34 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,38 @@
+<head>
+		<title>Jvilley</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+		
+	</head>
+	<body>
+
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="#">PSC MATE.ml</a></h1>
+						<nav class="links">
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Ipsum</a></li>
+								<li><a href="#">Feugiat</a></li>
+								<li><a href="#">Tempus</a></li>
+								<li><a href="#">Adipiscing</a></li>
+							</ul>
+						</nav>
+						<nav class="main">
+							<ul>
+								<li class="search">
+									<a class="fa-search" href="#search">Search</a>
+									<form id="search" method="get" action="#">
+										<input type="text" name="query" placeholder="Search" />
+									</form>
+								</li>
+								<li class="menu">
+									<a class="fa-bars" href="#menu">Menu</a>
+								</li>
+							</ul>
+						</nav>
+					</header>
